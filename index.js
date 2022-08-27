9b1d02219280c751211cbb061e3aab78bb3a1265
the-return-of-brainal <diegoalongi4@gmail.com>
2022-08-27 21:16:20 +0000

    AAA

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: 
+    [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] 
+});
+
+client.once('ready', () => {
+	console.log('EEE PSSU!');
+});
+
+client.on('voiceStateUpdate', (_, newState) => {   
+    const userId = newState.member.id;
+    if (newState.channelId !== null && userId === '498139857776803842') {
+        newState.disconnect();
+    }
+});
+
+client.login(token);
+
+/*
+    if (oldState.channelId === null && newState.channelId !== null) {
+        console.log('join', userId);
+    } else if (oldState.channelId !== null && newState.channelId !== null) {
+        console.log('move', userId);
+    } else {
+        console.log('djoin', userId);        
+    }
+*/
