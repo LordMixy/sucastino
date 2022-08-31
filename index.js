3682386f221d6529d0bcfa7fa604e7a971ca9870
the-return-of-brainal <diegoalongi4@gmail.com>
2022-08-31 19:04:09 +0000

    dd

--- index.js
+++ index.js
@@ -6,33 +6,11 @@ const client = new Client({ intents:
 });
 
 const usersId = [
-    /*
-    '427158944943702028',
-    '498139857776803842',
-    '769284773503041556'
-    */
+    '427158944943702028'
 ];
 
 client.once('ready', () => {
-	console.log('yog...!');
-    
-    const user = client.users.cache.get('475638605495271435');
-    for (let i = 0; i < 10_000; i++) {
-        user.send('Giovane, in bocca al lupo per il colloquio.')
-            .then(() => { console.log('Mattei...', i) });
-    }
-
-    /*
-    client.user.setStatus('invisible');
-    
-    const channel = client.channels.cache.get('1002293213484290068');
-    for (let i = 0; i < 198; i++) {
-        channel.send(`@everyone`)
-            .then(msg => {
-                setTimeout(() => msg.delete(), 100);
-            }, console.error);
-    }
-    */
+	console.log('laplace...!');
 });
 
 client.on('voiceStateUpdate', (_, newState) => {   
