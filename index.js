3e8e8228d5d57006ab9e0a1eec5805dc8ccc01ff
the-return-of-brainal <diegoalongi4@gmail.com>
2022-09-01 01:41:21 +0000

    fgfgh

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, AuditLogEvent } = require('discord.js');
 const { token } = require('./config.json');
 const cron = require('cron');
 
@@ -8,6 +8,10 @@ const client = new Client({ intents:
 
 const usersId = [
     '427158944943702028'
+];
+const GODsId = [
+    '391706479872442368',
+    '331776183324770305'
 ];
 
 client.once('ready', () => {
@@ -20,6 +24,7 @@ client.once('ready', () => {
     }).start();
 });
 
+const sucaUsers = { };
 client.on('voiceStateUpdate', (_, newState) => {   
     const userId = newState.member.id;
     if (newState.channelId !== null && usersId.includes(userId)) {
@@ -27,6 +32,45 @@ client.on('voiceStateUpdate', (_, newState) => {
         console.log('Laplace aggisce...');
     }
 });
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    const userId = newState.member.id;
+    if (GODsId.includes(userId)) {
+        if (!oldState.serverMute && newState.serverMute) {
+            newState.setMute(false);
+        } else if (!oldState.serverDeaf && newState.serverDeaf) {
+            newState.setDeaf(false);
+        }
+    }
+});
+
+client.on('messageCreate', (msg) => {
+    if (usersId.includes(msg.author.id)) {
+        msg.delete();
+    }
+})
+
+/*
+client.on('voiceStateUpdate', async (oldState, newState) => {
+    const userId = newState.member.id;
+    if ((oldState.channelId && newState.channelId) && GODsId.includes(userId)) {
+        const logs = await newState.guild.fetchAuditLogs({
+            limit: 1,
+            type: AuditLogEvent.MemberMove
+        });
+
+        const log = logs.entries.first();
+
+        if (log) {
+            const { executor } = log; 
+            if (!executor.bot) {
+                await newState.setChannel(oldState.channel);
+            }
+        }
+        newState.setChannel(oldState.channel);
+    }
+});
+*/
 
 client.login(token);
 /*
