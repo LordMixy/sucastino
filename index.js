42d0ba95e3c042656213b7274d1886d8e092f2ab
the-return-of-brainal <diegoalongi4@gmail.com>
2022-09-01 20:04:35 +0000

    aaaaa

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-const { Client, GatewayIntentBits, AuditLogEvent } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-const cron = require('cron');
 
-const client = new Client({ intents: 
-    [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences] 
+const client = new Client({ 
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences] 
 });
 
 const usersId = [
 	/* '427158944943702028' */
 ];
+
 const GODsId = [
     '391706479872442368',
-    '331776183324770305',
- '498139857776803842'
+    '331776183324770305'
 ];
 
 client.once('ready', () => {
     client.user.setStatus('invisible');
 	console.log('laplace...oh!');
-    
-    const user = client.users.cache.get('331776183324770305');
-    new cron.CronJob('0 0 13 * * *', () => {
-        user.send('Ricordati di accordare la tua chitarra, testa di cazzo!');
-    }).start();
 });
 
-const sucaUsers = { };
 client.on('voiceStateUpdate', (_, newState) => {   
     const userId = newState.member.id;
     if (newState.channelId !== null && usersId.includes(userId)) {
@@ -45,11 +38,19 @@ client.on('voiceStateUpdate', (oldState, newState) => {
     }
 });
 
+/*
+client.on('voiceStateUpdate', (oldState, newState) => {
+    if ((oldState.mute && !newState.mute) && newState.member.id === '498139857776803842') {
+        newState.setMute();
+    }
+});
+*/
+
 client.on('messageCreate', (msg) => {
     if (usersId.includes(msg.author.id)) {
         msg.delete();
     }
-})
+});
 
 /*
 client.on('voiceStateUpdate', async (oldState, newState) => {
@@ -74,12 +75,3 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
 */
 
 client.login(token);
-/*
-    if (oldState.channelId === null && newState.channelId !== null) {
-        console.log('join', userId);
-    } else if (oldState.channelId !== null && newState.channelId !== null) {
-        console.log('move', userId);
-    } else {
-        console.log('djoin', userId);        
-    }
-*/
